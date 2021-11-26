f0949462b8b7a7705db15ecd30b0d5c59df065b6
Nate Anderson <n8anderson@student.bridgew.edu>
2021-11-26 11:17:54 -0500

    gui functionality

--- gui.py
+++ gui.py
@@ -11,6 +11,16 @@ class Window(QMainWindow):
         # Labels
         self.title = 'Database Example'
 
+        self.studentID_Label = QLabel(self)
+
+        self.studentID = QLineEdit(self)
+
+        self.test_button = QPushButton(self)
+
+        # Pop ups
+        self.id_error = QMessageBox(self)
+        self.id_error.setWindowTitle("ID Error")
+        self.id_error.setText("The ID you have entered is invalid. Please enter a valid ID")
 
         # Database tools
         self.cursor = curs
@@ -23,7 +33,36 @@ class Window(QMainWindow):
         self.setWindowTitle("Test DB")
         self.setGeometry(50, 50, 800, 800)
 
+        self.studentID_Label.setText("Enter Student ID")
+        self.studentID_Label.resize(180, 30)
+        self.studentID_Label.move(50, 20)
+
+        self.studentID.move(50, 50)
+        self.studentID.resize(180, 30)
+
+        self.test_button.resize(180, 30)
+        self.test_button.move(50, 90)
+        self.test_button.setText('Select Information')
+        self.test_button.clicked.connect(self.test_button_connection)
 
 
         # Showing Ui
         self.show()
+
+    def test_button_connection(self):
+        table = 'enrollments'
+        studentID = self.studentID.text()
+        query = f"""SELECT EXISTS(SELECT 1 FROM Enrollments WHERE studentID = '{studentID}')"""
+        flag = self.cursor.execute(query).fetchall()[0][0]
+        if flag == 1:
+            query = f"""SELECT Enrollments.studentID, student_name, sectionID, course_desc, flags FROM
+                    Enrollments INNER JOIN Student S on S.studentID = Enrollments.studentID
+                    INNER JOIN Section Sec on sec.course_sectionID = sectionID
+                    INNER JOIN Course C on Sec.courseID = C.courseID WHERE Enrollments.studentID = '{studentID}'"""
+            self.cursor.execute(query)
+
+            df = pd.DataFrame.from_records(self.cursor.fetchall())
+
+            print(df)
+        else:
+            self.id_error.exec()
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import sqlite3 as sql
 
 def main():
     # Main database stuff here
-    conn, curs = sql.connect('north_star_db.db')
+    conn = sql.connect('north_star_db.db')
+    cursor = conn.cursor()
 
 
     # get_info(cursor, id) # May want to consider including this in GUI
     app = gui.QApplication(sys.argv)
-    ex = gui.Window(conn, curs)
+    ex = gui.Window(conn, cursor)
     ex.isHidden()
     sys.exit(app.exec_())
 
